70ba9af9e1582e1250f5a12a14653f3c60c3ec68
OldDad <jwhooper@gmail.com>
2017-06-11 23:53:40 -0400

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React, { Component } from 'react';
-import { observer } from 'mobx-react';
-import DevTools from 'mobx-react-devtools';
+import {observable, computed} from 'mobx';
+import {observer} from 'mobx-react';
+import React, {Component} from 'react';
 import ReactGridLayout from 'react-grid-layout';
 import './App.css';
+
+
+class tile {
+   @observable i;
+   @observable settings;
+   @observable p;
+   constructor(i,settings,p) {
+        this.i = i;
+        this.settings = settings;
+        this.p = p;
+    }
+ }
+
+ class gridlayout {
+    @observable tiles = [];
+ }
+
+const store = new gridlayout();
 
 @observer
 class App extends Component {
@@ -10,19 +28,23 @@ class App extends Component {
     return (
       <div>
         <h1>say something</h1>
-      <ReactGridLayout class="layout" cols={12} rowHeight={30} width={1200}>
-        <div key="a" data-grid={{x: 0, y: 0, w: 1, h: 2, static: true}}>a</div>
-        <div key="b" data-grid={{x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4}}>b</div>
-        <div key="c" data-grid={{x: 4, y: 0, w: 1, h: 2}}>c</div>
+      <ReactGridLayout class="layout" cols={12} 
+        rowHeight={30} width={1200}>
+        {store.tiles.map(tile => 
+          <div key={tile.i} data-grid={JSON.parse(tile.settings)}>{tile.p}</div>
+        )}
       </ReactGridLayout>
-        <DevTools />
       </div>
-    );
+    )
   }
+}
 
-  onReset = () => {
-    this.props.appState.resetTimer();
-  }
-};
+store.tiles.push(
+  new tile(0,'{x: 0, y: 0, w: 1, h: 2}','comp1'),
+  new tile(1,'{x: 3, y: 1, w: 1, h: 2}','comp2'),
+  new tile(2,'{x: 5, y: 2, w: 1, h: 2}','comp3')
+);
+
+console.log(store.tiles)
 
 export default App;
